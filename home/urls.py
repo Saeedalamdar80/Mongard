55655c7529c35b53d70b629f9789921e58c06a3f
saeedalam <saeedalam@SAEED.localdomain>
2025-10-27 16:27:08 +0330

    add delete button

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
-    path('play/', views.say_hello),
-    path('home/detail/todo/', views.detail),
-    
+    path('home/', views.home, name='home'),
+    path('detail/<int:todo_id>/', views.detail, name= 'details'), # url dispatcher
+    path('delete/<int:todo_id>/', views.delete, name='delete'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,14 @@
 from .models import Todo
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 def home(request):
     all = Todo.objects.all()
     return render(request, 'home.html', {'todos': all})
 
-def say_hello(request):
-   
-    return render(request, 'hello.html', {'name': 'javad'})
-
-
 def detail(request, todo_id):
     todo = Todo.objects.get(id = todo_id)
-    return render(request, 'detail.html', {'': todo})
+    return render(request, 'detail.html', {'todo': todo})
+
+def delete(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return redirect('home') 
